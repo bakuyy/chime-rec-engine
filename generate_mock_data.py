32080a7233f63e2218cac3d81cfe24c6861f3011
bakuyy <sophieyang12345@gmail.com>
2025-01-18 01:10:48 -0500

    Created mock data function for collaborative filter testing

---
+++ generate_mock_data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+def create_mock_interactions(n_users, n_songs, sparsity):
+    '''
+    create mock data for user interactions with songs
+    parameters:
+        - n_users = number of users 
+        - n_songs = number of songs in dataset
+        - sparsity = ratio of possible interactions to generate (range of 0,1)
+    '''
+
+    total_possible_interactions = n_users * n_songs
+    num_interactions = int(total_possible_interactions * sparsity)
+
+    # p.random.randint takes in 3 paramters below: (low, high, size)
+    # (lower bound, upper bound, how many numbers to generate)
+    # for chime: the interactions will be 0 = dislike, 1 = like, 2 = chime
+    interactions = pd.DataFrame({
+        "user_id": np.random.randint(0, n_users, num_interactions),
+        "song_id" : np.random.randint(0, n_songs, num_interactions),
+        "interaction": np.random.choice([0,1,2], num_interactions,p=[0.3,0.4,0.3]),
+        "listen_count":np.random.choice(range(1,51), num_interactions, p=np.linspace(0.5, 0.01, 50)/sum(np.linspace(0.5, 0.01, 50))),
+        "timestamp":[datetime.now() - timedelta(days=np.random.randint(0, 365)) 
+                     for _ in range(num_interactions)]
+    })
+
+    #removes duplicates for user-song combinations (same user can't rate the same song more than once)
+    interactions = interactions.drop_duplicates(subset=['user_id', 'song_id'])
+
+    #data
+    return interactions.sort_values(['user_id','timestamp'])
+
+#mock_data
+interactions_df = create_mock_interactions(100,500,0.1)
+
+#generate user-item matrix
+user_item_matrix = interactions_df.pivot(
+    index='user_id', 
+    columns='song_id', 
+    values='interaction'
+).fillna(0)
+
+print(interactions_df.head())
+
+
